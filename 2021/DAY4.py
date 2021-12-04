6e76881b26ac1bb34657a7938873fdf246578b99
Thu Nguyen <thu.nguyen@thistle.co>
2021-12-04 03:30:32 -0800

    Solution for DAY 4 PART 2 AoC

--- 2021/DAY4.py
+++ 2021/DAY4.py
@@ -1,12 +1,13 @@
+from re import L
 import numpy as np
 
 
 class Board():
     def __init__(self, board):
+        board = np.array(board)
         self.board = board
         self.rows = [set(row) for row in board]
         self.columns = [set(col) for col in board.transpose()]
-
 
     def check_num(self, num):
         for row in self.rows:
@@ -16,12 +17,10 @@ class Board():
                 if num in col: col.remove(num)
         return self.check_row(num) or self.check_column(num)
 
-
     def check_row(self, num):
         for row in self.rows:
             if len(row) == 0:
                 return sum(list(map(sum, self.rows))) * num
-
 
     def check_column(self, num):
         for col in self.columns:
@@ -33,10 +32,9 @@ file = open('04.txt', 'r')
 text = file.read()
 lines = text.split('\n\n')
 draws, *matrices = lines
+matrices = [[list(map(int, row.split())) for row in matrix.split('\n')] for matrix in matrices]
 
 draws = list(map(int, draws.split(',')))
-matrices = [np.array([list(map(int, row.split())) for row in matrix.split('\n')]) for matrix in matrices]
-
 boards = [Board(board=matrix) for matrix in matrices]
 
 # PART 1
@@ -47,4 +45,19 @@ def play_bingo_part_1():
             if bingo:
                 return bingo
 
+# PART 2
+def play_bingo_part_2():
+    wins = set()
+    for draw in draws:
+        for i in range(len(boards)):
+            if i not in wins:
+                bingo = boards[i].check_num(num=draw)
+
+                if bingo:
+                    wins.add(i)
+
+                if len(wins) == len(boards):
+                    return bingo
+
 print('DAY 4 | PART 1:', play_bingo_part_1())
+print('DAY 4 | PART 2:', play_bingo_part_2())
