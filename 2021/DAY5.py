dbbccf51d243ddf39ffce1160d080043223b6bc2
Thu Nguyen <thu.nguyen@thistle.co>
2021-12-05 13:17:27 -0800

    Simplify solution with a mapper method that overlaps() calls

--- 2021/DAY5.py
+++ 2021/DAY5.py
@@ -4,14 +4,14 @@ with open('05.txt') as file:
     coordinates = [[(int(x1), int(y1)), (int(x2), int(y2))] for (x1, y1), (x2, y2) in data]
     vertizontals = list(filter(lambda x: x[0][0] == x[1][0] or x[0][1] == x[1][1], coordinates))
 
-
 class Diagram():
-    def __init__(self, bounds):
-        self.Xmax = sorted([x1 for (x1, _), _ in bounds] + [x2 for _, (x2, _) in bounds])[-1]
-        self.Ymax = sorted([y1 for (_, y1), _ in bounds] + [y2 for _, (_, y2) in bounds])[-1]
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+        self.Xmax = sorted([x1 for (x1, _), _ in coordinates] + [x2 for _, (x2, _) in coordinates])[-1]
+        self.Ymax = sorted([y1 for (_, y1), _ in coordinates] + [y2 for _, (_, y2) in coordinates])[-1]
         self.grid = [[0 for _ in range(self.Xmax + 1)] for _ in range(self.Ymax + 1)]
 
-    def mark(self, endpoints):
+    def linefill(self, endpoints):
         (x1, y1), (x2, y2) = sorted(endpoints)
         if x1 == x2:
             for y in range(y1, y2+1):
@@ -25,16 +25,16 @@ class Diagram():
             for x, y in zip(Xs, Ys):
                 self.grid[y][x] += 1
 
+    def mapper(self):
+        for coords in self.coordinates:
+            self.linefill(coords)
+
     def overlaps(self):
+        self.mapper()
         return len([num for row in self.grid for num in row if num > 1])
 
-
 diagram_part_1 = Diagram(vertizontals)
-for coords in vertizontals:
-    diagram_part_1.mark(coords)
-print(f'DAY 5 | PART 1: {diagram_part_1.overlaps()}')
-
 diagram_part_2 = Diagram(coordinates)
-for coords in coordinates:
-    diagram_part_2.mark(coords)
+
+print(f'DAY 5 | PART 1: {diagram_part_1.overlaps()}')
 print(f'DAY 5 | PART 2: {diagram_part_2.overlaps()}')
