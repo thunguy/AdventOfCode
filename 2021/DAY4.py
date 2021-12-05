3a8358b62e07584c9833d03166aab3d84558058e
Thu Nguyen <thu.nguyen@thistle.co>
2021-12-04 18:33:26 -0800

    Combine part 1 and part 2

--- 2021/DAY4.py
+++ 2021/DAY4.py
@@ -2,7 +2,7 @@ class Board():
     def __init__(self, board):
         self.board = board
         self.rows = [set(row) for row in board]
-        self.columns = [set(row) for row in zip(*board)]
+        self.columns = [set(col) for col in zip(*board)]
 
     def check_num(self, num):
         for row in self.rows:
@@ -21,33 +21,48 @@ class Board():
 
 
 file = open('04.txt', 'r')
-text = file.read()
-lines = text.split('\n\n')
+lines = file.read().split('\n\n')
 draws, *matrices = lines
 matrices = [[list(map(int, row.split())) for row in matrix.split('\n')] for matrix in matrices]
 
 draws = list(map(int, draws.split(',')))
 boards = [Board(board=matrix) for matrix in matrices]
 
-# PART 1
-def play_bingo_part_1():
-    for draw in draws:
-        for board in boards:
+bingos, scores, num_boards = set(), list(), len(boards)
+
+for draw in draws:
+    for i, board in enumerate(boards):
+        if i not in bingos:
             score = board.check_num(draw)
             if score:
-                return score
+                bingos.add(i)
+                scores.append(score)
+            if len(bingos) == num_boards:
+                break
 
-# PART 2
-def play_bingo_part_2():
-    bingos, num_boards = set(), len(boards)
-    for draw in draws:
-        for i, board in enumerate(boards):
-            if i not in bingos:
-                score = board.check_num(draw)
-                if score:
-                    bingos.add(i)
-                if len(bingos) == num_boards:
-                    return score
+print(f'DAY 4 | PART 1: {scores[0]}')
+print(f'DAY 4 | PART 2: {scores[-1]}')
 
-print('DAY 4 | PART 1:', play_bingo_part_1())
-print('DAY 4 | PART 2:', play_bingo_part_2())
+
+# # PART 1
+# def play_bingo_part_1():
+#     for draw in draws:
+#         for board in boards:
+#             score = board.check_num(draw)
+#             if score:
+#                 return score
+
+# # PART 2
+# def play_bingo_part_2():
+#     bingos, num_boards = set(), len(boards)
+#     for draw in draws:
+#         for i, board in enumerate(boards):
+#             if i not in bingos:
+#                 score = board.check_num(draw)
+#                 if score:
+#                     bingos.add(i)
+#                 if len(bingos) == num_boards:
+#                     return score
+
+# print('DAY 4 | PART 1:', play_bingo_part_1())
+# print('DAY 4 | PART 2:', play_bingo_part_2())
---
+++ 2021/DAY5.py
@@ -0,0 +1,12 @@
+with open('05.txt') as file:
+    lines = file.readlines()
+    coordinates = [line.split(' -> ') for line in lines]
+    print(coordinates)
+def part_1():
+    pass
+
+def part_2():
+    pass
+
+# print(f'DAY 5 | PART 1: {}')
+# print(f'DAY 5 | PART 2: {}')
