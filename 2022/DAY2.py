b3287966586b7e5e977760af2dc30e9904ae9fd8
Thu Nguyen <thunguy@me.com>
2022-12-02 06:27:14 -0800

    Dry up DAY2 solution

--- 2022/DAY2.py
+++ 2022/DAY2.py
@@ -1,23 +1,21 @@
 with open("inputs/02.txt") as file:
     points = {'X': 1, 'Y': 2, 'Z': 3}
-    shapes = {'A': 'X', 'B': 'Y', 'C': 'Z'}
-    rounds = [(shapes[a], b) for a, b in [line.split() for line in file]]
-    events = {'X': {'superior': 'Y', 'inferior': 'Z'},
+    mapper = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+    rounds = [(mapper[a], b) for a, b in [line.split() for line in file]]
+    shapes = {'X': {'superior': 'Y', 'inferior': 'Z'},
               'Y': {'superior': 'Z', 'inferior': 'X'},
               'Z': {'superior': 'X', 'inferior': 'Y'}}
 
-score = 0
-for opponent, mine in rounds:
-    score += points[mine]
+def get_round_score(opponent, mine):
+    score = points[mine]
     score += 3 if mine == opponent else \
-             6 if mine == events[opponent]['superior'] else \
+             6 if mine == shapes[opponent]['superior'] else \
              0
-print(f'DAY 2 | PART 1: {score}')
+    return score
 
-score = 0
-lose, draw, win = 'X', 'Y', 'Z'
-for opponent, outcome in rounds:
-    score += 3 + points[opponent] if outcome == draw else \
-             6 + points[events[opponent]['superior']] if outcome == win else \
-             0 + points[events[opponent]['inferior']]
-print(f'DAY 2 | PART 2: {score}')
+outcomes = {'X': {k:v['inferior'] for k,v in shapes.items()},
+            'Y': {k:k for k,_ in shapes.items()},
+            'Z': {k:v['superior'] for k,v in shapes.items()}}
+
+print(f'DAY 2 | PART 1: {sum([get_round_score(opponent, mine) for opponent, mine in rounds])}')
+print(f'DAY 2 | PART 2: {sum([get_round_score(opponent, outcomes[mine][opponent]) for opponent, mine in rounds])}')
