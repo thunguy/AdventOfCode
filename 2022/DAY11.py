8664be60124f65b563952210d5e6209640a33dc7
Thu Nguyen <thunguy@me.com>
2022-12-12 15:41:24 -0800

    Add variable mapping note

--- 2022/DAY11.py
+++ 2022/DAY11.py
@@ -1,26 +1,23 @@
 from math import prod
 
-
 FILE = open('inputs/11.txt').read().strip().splitlines()
 NOTES, MONKEYS, DIVISOR = None, None, None
-
 
 def parse_notes(file=FILE) -> list:
     global NOTES, DIVISOR, MONKEYS
     NOTES = [[
-        [int(n) for n in a.split(', ')],
-        b.split('= ')[1],
-        int(c.split('by ')[-1]),
-        int(d.split()[-1]),
-        int(e.split()[-1]),
-    ] for a, b, c, d, e in [
+        [int(n) for n in I.split(', ')],  # I: starting items
+        O.split('= ')[1],                 # O: operation
+        int(D.split('by ')[-1]),          # D: test denominator
+        int(T.split()[-1]),               # T: receiving monkey if test is True
+        int(F.split()[-1]),               # F: receiving monkey if test is False
+    ] for I, O, D, T, F in [
         [l.split(': ')[1] for l in file[n:n+7][1:] if l]
         for n in range(0, len(file), 7)
     ]]
     DIVISOR = prod([n[2] for n in NOTES])
     MONKEYS = len(NOTES)
     return NOTES
-
 
 def monkey_business(rounds: int, scared: bool) -> int:
     notes, levels = parse_notes(), [0] * MONKEYS
@@ -34,7 +31,6 @@ def monkey_business(rounds: int, scared: bool) -> int:
                 levels[M] += 1
     levels.sort()
     return prod(levels[-2:])
-
 
 print(f'DAY 11 | PART 1: {monkey_business(20, False)}')
 print(f'DAY 11 | PART 2: {monkey_business(10000, True)}')
--- 2022/DAY8.py
+++ 2022/DAY8.py
@@ -2,16 +2,15 @@ trees = open('inputs/08.txt').read().splitlines()
 rows = [[int(t) for t in row] for row in trees]
 cols = [[t for t in row] for row in zip(*[row for row in rows])]
 
+# H = height, L = left, R = right, U = up, D = down
 
 def treepaths(X, Y, row, col):
     L, R = row[:Y], row[1+Y:]
     U, D = col[:X], col[1+X:]
     return dict(L=L, R=R, U=U, D=D)
 
-
 def is_hidden(H, L, R, U, D):
     return H <= min(max(L), max(R), max(U), max(D))
-
 
 def how_scenic(H, L, R, U, D):
     directions = [L[::-1], R, U[::-1], D]
@@ -22,7 +21,6 @@ def how_scenic(H, L, R, U, D):
                 score *= (j+1)
                 break
     return score
-
 
 def count_and_score_visible_trees(tree_count=0,
                                   best_score=0):
@@ -43,7 +41,6 @@ def count_and_score_visible_trees(tree_count=0,
             tree_count += int(not hidden)
             best_score = max(best_score, score)
     return tree_count, best_score
-
 
 count, score = count_and_score_visible_trees()
 print(f'DAY 8 | PART 1: {count}')
